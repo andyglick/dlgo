5c8e23873a68a9a9b4d0c6b5b27f361bfd2068ca
Andy Glick <andyglick@gmail.com>
2020-04-08 22:12:41 -0400

    Initial commit

---
+++ game_state_new.py
@@ -0,0 +1,12 @@
+from dlgo import goboard_slow
+from dlgo.utils import print_board, print_move
+
+
+def main():
+    board_size = 9
+    game = goboard_slow.GameState.new_game(board_size)
+    print_board(game.board)
+
+
+if __name__ == '__main__':
+    main()
